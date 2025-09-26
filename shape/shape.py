978e629e8d9be3a265baf4319c295d48f29ba716
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2025-09-26 09:17:59 -0500

    Implemented abstract shape class with init, str and abstract method

--- shape/shape.py
+++ shape/shape.py
@@ -1,4 +1,54 @@
 """This module defines the Shape class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Komalpreet Kaur"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    """
+    Abstract base class representing a shape with a color.
+    """
+
+    def __init__(self, color: str):
+        """
+        Initializes a Shape object based upon received arguments (if valid).
+
+        Args:
+            color (str): The color of the shape.
+
+        Raises:
+            ValueError: If color is blank.
+        """
+        if len(color.strip()) == 0:
+            raise ValueError("Color cannot be blank.")
+        self._color = color
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the Shape.
+
+        Returns:
+            str: Description of the shape's color.
+        """
+        return f"The shape color is {self._color}."
+    
+    @abstractmethod
+    def calculate_area(self):
+        """
+        Abstract method to calculate the area of the shape.
+
+        Returns:
+            float: The area of the shape.
+        """
+        pass
+
+    @abstractmethod
+    def calculate_parameter(self):
+        """
+        Abstract method to calculate the perimeter of the shape.
+
+        Returns:
+            float: The perimeter of the shape.
+        """
+        pass
