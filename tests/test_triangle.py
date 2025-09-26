a34d31cdb99e72fd5310954ce2e5d3a28df0f74f
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2025-09-26 10:35:47 -0500

    Implemented unit tests for Triangle with try/except

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -6,7 +6,84 @@ Usage:
     $ python -m unittest tests/test_triangle.py
 """
 
-__author__ = ""
-__version__ = ""
+__author__ = "Komalpreet Kaur"
+__version__ = "1.0.0"
 
 import unittest
+from shape.triangle import Triangle
+
+def main():
+    """ Attribute set to input value."""
+    try:
+        Triangle("red", 5, 6, 7)
+    except ValueError as e:
+        print(e)
+
+    """Exception raised when color is blank."""
+    try:
+        Triangle(" ", 5, 6, 7)
+    except ValueError as e:
+        print(e)
+
+    """Exception raised when side_1 is not an integer(string)."""
+    try:
+        Triangle("red", "five", 6, 7)
+    except ValueError as e:
+        print(e)
+
+    """Exception raised when side_1 is not an integer(float)."""
+    try:
+        Triangle("red", 5.5, 6, 7)
+    except ValueError as e:
+        print(e)
+
+    """Exception raised when side_2 is not an integer(string)."""
+    try:
+        Triangle("red", 5, "six", 7)
+    except ValueError as e:
+        print(e)
+
+    """Exception raised when side_2 is not an integer(float)."""
+    try:
+        Triangle("red", 5, 6.5, 7)
+    except ValueError as e:
+        print(e)
+
+    """Exception raised when side_3 is not an integer(string)."""
+    try:
+        Triangle("red", 5, 6, "seven")
+    except ValueError as e:
+        print(e)
+
+    """Exception raised when side_3 is not an integer(float)."""
+    try:
+        Triangle("red", 5, 6, 7.5)
+    except ValueError as e:
+        print(e)
+
+    """Exception raised when sides do not satisfy Triangle Inequality Theorem."""
+    try:
+        Triangle("red", 1, 2, 3)
+    except ValueError as e:
+        print(e)
+
+    """Returns string format appropriately."""
+    try:
+        print(Triangle("red", 5, 6, 7))
+    except ValueError as e:
+        print(e)
+
+    """Returns correct value for calculated_area."""
+    try:
+        print(Triangle("red", 3, 4, 5).calculate_area())
+    except ValueError as e:
+        print(e)
+
+    """Returns correct value for calculate_perimeter."""
+    try:
+        print(Triangle("red", 3, 4, 5).calculate_parameter())
+    except ValueError as e:
+        print(e)
+
+main()
+
